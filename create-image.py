31a33d33a3fdd12d50e04a68740291a86fc5a52f
schleising <stephen@schleising.net>
2022-02-27 12:41:42 +0000

    Save the output

--- create-image.py
+++ create-image.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from PIL import Image, ImageDraw, ImageFont
 
 font = ImageFont.truetype("Roboto/Roboto-Regular.ttf", 48)
@@ -6,5 +8,7 @@ d = ImageDraw.Draw(im)
 d.line(((0, 100), (200, 100)), "blue")
 d.line(((100, 0), (100, 200)), "blue")
 d.text((100, 100), "Quick", fill="red", anchor='ms', font=font)
+
+im.save(Path('output/Quick.png'))
 
 im.show()
