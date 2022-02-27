b4bcb194a79c12471c58106b2f79359ab32754c5
schleising <stephen@schleising.net>
2022-02-27 11:25:30 +0000

    Use open as a context manager

--- read_image.py
+++ read_image.py
@@ -2,10 +2,10 @@ from pathlib import Path
 
 from PIL import Image
 
-im = Image.open(Path('input/test_image01.jpg'))
+with Image.open(Path('input/test_image01.jpg')) as im:
+    print(im.format, im.size, im.mode)
 
-print(im.format, im.size, im.mode)
+    r, g, b = im.split()
+    im = Image.merge('RGB', (b, r, g))
 
-im = im.rotate(270)
-
-im.show()
+    im.show()
